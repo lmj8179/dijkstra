9d05da9c5bdcab9dd12038f503dcd4b79d08f893
Manoel Vilela <manoel_vilela@engineer.com>
2015-07-21 18:46:00 -0300

    Added a header for main code

--- C/dijkstra.c
+++ C/dijkstra.c
@@ -1,10 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *                      Copyright 2015 Manoel Vilela
+ *
+ *
+ *       Filename:  dijkstra.c
+ *
+ *    Description:  A implementation of dijkstra's algorithm in ANSI C
+ *
+ *         Author:  Manoel Vilela
+ *        Contact:  manoel_vilela@engineer.com
+ *   Organization:  UFPA
+ *
+ * =====================================================================================
+**/
+
 /*===========================================================
  *
  *  -*-         The end of universe begin here          -*-
  *
  *===========================================================
  */
-
 
  int main(int argc, char *argv[]){
 
